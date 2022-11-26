62acca440c34d43f03bb5993b070c01d6766cb25
Skudle <113626842+Skudle@users.noreply.github.com>
2022-11-26 16:44:10 +0100

    Add files via upload

--- mission9.py
+++ mission9.py
@@ -99,6 +99,9 @@ class Facture:
         return self.__description
 
     def articles(self):
+        """
+        :return: retourne la liste d'objets de type Article
+        """
         return self.__articles
 
     def __str__(self):
@@ -148,9 +151,9 @@ class Facture:
 
     def totaux_str_pkpp(self, nombre, poids):
         """
-        @pre:  prix un float représentant le prix total de la facture en EURO
-               tva un float représentant le TVA total de la facture en EURO
-        @post: retourne un string représentant une ligne de facture avec les totaux prix et tva,
+        @pre:  nombre est le nombre total de tout les articles combiné
+               poids est l'addition des poids de tout les articles
+        @post: retourne un string représentant une ligne de facture avec les nombre totaux et poids,
                à imprimer en bas de la facture
         """
         return self.barre_str() \
@@ -173,13 +176,6 @@ class Facture:
             else:
                 continue
         return same_piece
-
-    def get_num_for_article(self, article_obj_class):
-        count = 0
-        for word in self.articles():
-            if word == article_obj_class:
-                count += 1
-        return count
 
 
     # Cette méthode doit être ajouté lors de l'étape 2.6 de la mission    
@@ -206,11 +202,22 @@ class Facture:
         @pre:  art une instance de la classe Article
         @post: retourne un string correspondant à une ligne de facture pour l'article art
         """
-        print(art.piece_obj.poids())
         return "| {0:40} | {1:10.3f}kg | {2:10.0f} |{3:8.3f}kg|\n".format(art.piece_obj.description(), art.piece_obj.poids(), art.nombre(),
+                                                                         art.poids_total())
+    def article_str_pkpp_test(self, art):
+        """
+        @pre:  art une instance de la classe Article
+        @post: retourne un string correspondant à une ligne de facture pour l'article art
+        """
+        return "| {0:40} | {1:10.3f}kg | {2:10.0f} |{3:8.3f}kg|\n".format(art.description_sans_nombre(), art.piece_obj.poids(), art.nombre(),
                                                                          art.poids_total())
 
     def print_livraison(self):
+        """
+        args: un objet de class facture
+        creé une facture avec les données de l'objet
+        retourne une facture à imprimer avec tous les articles, poids, prix, livraison fragile, ...
+        """
         s = self.livraison_str()
         poids = 0
         total_nombre = 0
@@ -230,7 +237,32 @@ class Facture:
         s += self.totaux_str_pkpp(total_nombre, poids)
         return s
 
+    def print_livraison_avec_prix(self):
+        """
+        args: un objet de class facture
+        creé une facture avec les données de l'objet
+        retourne une facture à imprimer avec tous les articles, poids, prix, livraison fragile, ... mais cette fois avec les prix à coté
+        des articles dans la facture
 
+        """
+        s = self.livraison_str()
+        poids = 0
+        total_nombre = 0
+        article_fragile = 0
+        for art in self.articles():
+            if art.piece_obj.fragile() == True:
+                article_fragile += 1
+            s += self.article_str_pkpp_test(art)
+            poids += art.poids_total()
+            total_nombre += art.nombre()
+
+        total_nombre = int(total_nombre)
+        if article_fragile >= 1:
+            s += self.totaux_str_pkpp(total_nombre, poids)
+            s += " (!) *** livraison fragile ***"
+            return s
+        s += self.totaux_str_pkpp(total_nombre, poids)
+        return s
 #########################
 ### ARTICLEREPARATION ###
 #########################
@@ -239,12 +271,27 @@ class ArticleReparation(Article):
     def __init__(self, d, p, duration: float):
         super().__init__(d, p)
         self.duration = duration
+        if self.duration < 0:
+            raise ValueError
+        else:
+            pass
 
     def description(self):
+        """
+        retourne la description de la reparation
+
+        """
         return f"Réparation ({self.duration} heures)"
 
     def prix(self):
+        """
+        retourne le prix total de la reparation
+        """
         return 20 + (35*self.duration)
+
+#############
+### PIECE ###
+#############
 
 class Piece:
     def __init__(self, string: str, montant: float, kg: float, fragilité=False, taux_tva_reduite=False):
@@ -257,7 +304,16 @@ class Piece:
         if self.__fragilité == True:
             self.__string += "(!)"
 
+    def __str__(self):
+        """
+        retourne un string avec tous les attributs de la classe piece
+        """
+        return f"{self.description()} - {str(self.prix())} - {str(self.poids())} - {str(self.fragile())} - {str(self.tva_reduit())}"
+
     def description(self):
+        """
+        retourne la description textuelle de l'article piece
+        """
         return self.__string
 
     def prix(self):
@@ -269,12 +325,16 @@ class Piece:
     def fragile(self):
         return self.__fragilité
 
-    def tva_reduite(self):
+    def tva_reduit(self):
         return self.__taux_tva_reduite
 
     def __eq__(self, other):
-        return self.description() == other.description() and self.prix() == other.prix()
+        return self.description() == other.description() and self.prix() == other.prix():
 
+
+####################
+### ARTICLEPIECE ###
+####################
 
 class ArticlePiece(Article):
     def __init__(self, d: str, p: float, piece_number: int, piece_obj):
@@ -282,63 +342,66 @@ class ArticlePiece(Article):
         self.__number = piece_number
         self.piece_obj = piece_obj
 
+
     def super_description(self):
+        """
+        apelle la methode description de la class parent (article)
+        """
         return super().description()
 
     def tva_ou_pas(self):
-        return self.piece_obj.tva_reduite()
+        """
+        retourne vrai si l'objet piece beneficie d'une reduction de tva sinon false
+
+        """
+        return self.piece_obj.tva_reduit()
 
     def nombre(self):
         return self.__number
 
     def prix_piece(self):
+        '''
+        retourne le prix de l'article
+        '''
         return super().prix()
 
     def description(self):
+        """
+        retourne la description de l'article
+        """
         return f"{self.nombre()} * {super().description()} @ {float(super().prix())}"
 
+    def description_sans_nombre(self):
+        """
+        retourne la description de l'article sans les nombre à cote de l'article
+        """
+        return f"{self.piece_obj.description()} @ {format(float(super().prix()), '.2f')}"
+
     def prix(self):
+        """
+        retourne le prix total de l'article HTVA
+
+        """
         return self.nombre() * super().prix()
 
     def taux_tva(self):
-        if self.piece_obj.tva_reduite() == True:
+        '''
+        :return: 0.06 si l'article beneficie d'une reduc tva sinon 0.21
+        '''
+        if self.piece_obj.tva_reduit() == True:
             return 0.06
         else:
             return 0.21
 
     def poids_total(self):
+        """
+        retourne le poids total de l'article
+
+        """
         return self.nombre() * self.piece_obj.poids()
 
-#string: str, montant: float, kg: float, fragilité=False, taux_tva_reduite=False):
-#d: str, p: float, piece_number: int, piece_obj):
-carte_graphique_piece = Piece("Carte Graphique RTX 3090", 1599.99, 2, True, False)
-disque_dur_piece = Piece("disque dur 350 GB", 5,  0.355, True, True)
-adaptateur_DVI_VGA = Piece("adaptateur DVI - VGA ", 12.0, 0)
-adaptateur_DVI_VGA_article = ArticlePiece("adaptateur DVI - VGA ", 12.0, 5, adaptateur_DVI_VGA)
-Java_in_a_Nutshell = Piece("Java in a Nutshell", 24.00, 0.321)
-Java_in_a_Nutshell_article = ArticlePiece("Java in a Nutshell", 24.0, 2, Java_in_a_Nutshell)
-disque_dur_article_piece = ArticlePiece("disque dur 350 GB", 5, 1, disque_dur_piece)
-souris_bluetooth = Piece("souris bluetooth", 15.99, 0.176, False, False)
-souris_bluetooth_article_piece = ArticlePiece("souris bluetooth", 15.99, 3, souris_bluetooth)
-souris_bluetooth_article_piece2 = ArticlePiece("souris bluetooth", 15.99, 5, souris_bluetooth)
-pc_store = Facture("PC store 22 octobre", [disque_dur_article_piece, souris_bluetooth_article_piece, adaptateur_DVI_VGA_article, Java_in_a_Nutshell_article, souris_bluetooth_article_piece2], 1)
-print(pc_store)
-#g2.super_edit_description('ttt')
-#print(Article.description(g2))
-
-
-
-#############
-### PIECE ###
-#############
-
-# Cette classe doit être ajouté lors de l'étape 2.3 de la mission    
-
-####################
-### ARTICLEPIECE ###
-####################
-
-
+    def __str__(self):
+        return f"{self.super_description()} - {self.prix_piece()} - {self.piece_obj.description()}"
 
 
 ########################
@@ -347,3 +410,18 @@ print(pc_store)
 
 # Ajouter votre code ici pour imprimer une facture et un borderaux
 # de livraison.
+if __name__ == '__main__':
+    carte_graphique_piece2 = Piece("Cparte Graphique RTX 3090", 1599.99, 2, True, False)
+    carte_graphique_piece = Piece("Carte Graphique RTX 3090", 1599.99, 2, True, False)
+    disque_dur_piece = Piece("disque dur 350 GB", 5, 0.355, True, True)
+    adaptateur_DVI_VGA = Piece("adaptateur DVI - VGA ", 12.0, 0)
+    adaptateur_DVI_VGA_article = ArticlePiece("adaptateur DVI - VGA ", 12.0, 5, adaptateur_DVI_VGA)
+    Java_in_a_Nutshell = Piece("Java in a Nutshell", 24.00, 0.321)
+    Java_in_a_Nutshell_article = ArticlePiece("Java in a Nutshell", 24.0, 2, Java_in_a_Nutshell)
+    disque_dur_article_piece = ArticlePiece("disque dur 350 GB", 5, 1, disque_dur_piece)
+    souris_bluetooth = Piece("souris bluetooth", 15.99, 0.176, False, False)
+    souris_bluetooth_article_piece = ArticlePiece("souris bluetooth", 15.99, 3, souris_bluetooth)
+    souris_bluetooth_article_piece2 = ArticlePiece("souris bluetooth", 15.99, 5, souris_bluetooth)
+    pc_store = Facture("PC store 22 octobre",[disque_dur_article_piece, souris_bluetooth_article_piece, adaptateur_DVI_VGA_article, Java_in_a_Nutshell_article, souris_bluetooth_article_piece2], 1)
+    print(pc_store.print_livraison())
+
--- test.py
+++ test.py
@@ -19,6 +19,17 @@ articles = [ Article("laptop 15\" 8GB RAM", 743.79),
              ArticlePiece("souris bluetooth", 15.99, 3, Piece("souris bluettoth", 15.99, 0.5, True, False)),
              ArticlePiece("Java in a Nutshell", 24, 2, Piece("Java in a Nutshell", 24, 0.1, False, True))
              ]
+carte_graphique_piece = Piece("Carte Graphique RTX 3090", 1599.99, 2, True, False)
+disque_dur_piece = Piece("disque dur 350 GB", 5,  0.355, True, True)
+adaptateur_DVI_VGA = Piece("adaptateur DVI - VGA ", 12.0, 0)
+adaptateur_DVI_VGA_article = ArticlePiece("adaptateur DVI - VGA ", 12.0, 5, adaptateur_DVI_VGA)
+Java_in_a_Nutshell = Piece("Java in a Nutshell", 24.00, 0.321)
+Java_in_a_Nutshell_article = ArticlePiece("Java in a Nutshell", 24.0, 2, Java_in_a_Nutshell)
+disque_dur_article_piece = ArticlePiece("disque dur 350 GB", 5, 1, disque_dur_piece)
+souris_bluetooth = Piece("souris bluetooth", 15.99, 0.176, False, False)
+souris_bluetooth_article_piece = ArticlePiece("souris bluetooth", 15.99, 3, souris_bluetooth)
+souris_bluetooth_article_piece2 = ArticlePiece("souris bluetooth", 15.99, 5, souris_bluetooth)
+pc_store = Facture("PC store 22 octobre", [disque_dur_article_piece, souris_bluetooth_article_piece, adaptateur_DVI_VGA_article, Java_in_a_Nutshell_article, souris_bluetooth_article_piece2], 1)
 
 def test_articles(a_list) :
     for art in a_list :
@@ -29,10 +40,13 @@ Test initial pour la classe Facture.
 @author Kim Mens
 @version 4 novembre 2020
 """
-facture = Facture("PC Store - 22 novembre", articles)
+facture = Facture("PC Store - 22 novembre", articles, 1)
 
 def test_facture(f) :
     print(f)
+def test_facture_pp(f):
+    print(f.print_livraison_avec_prix())
+
 
 """
 Faire exécuter les différents tests.
@@ -40,13 +54,42 @@ Faire exécuter les différents tests.
 a = ArticleReparation("Acer Nitro 5", 1099, 2)
 a2 = ArticleReparation("Acer Nitro 5", 1099, 0)
 
+
 def test_article_reparation():
     assert a.description() == "Réparation (2 heures)"
     assert a.prix() == 90
     assert a2.prix() == 20
+    assert a.prix() == 90
+    try:
+        a3 = ArticleReparation("Acer Nitro 5", 1099, -1)
+    except ValueError:
+        print("Test passed") #La durée de répération ne peut etre inferieur à 0
+    assert a2.prix() == 20
 
-'''def test_article_piece():
-    assert'''
+
+b = Piece("RPI 4", 65.99, 0.120, True, True)
+b2 = Piece("RPI 4", 65.99, 0.120, False, False)
+
+def test_piece():
+    assert b.__str__() == "RPI 4(!) - 65.99 - 0.12 - True - True"
+    assert b2.__str__() == "RPI 4 - 65.99 - 0.12 - False - False"
+
+c = ArticlePiece("RPI 4", 65.99, 2, b2)
+c2 = ArticlePiece("RPI 4", 65.99, 0, b)
+
+
+def test_article_piece():
+    assert c.__str__() == "RPI 4 - 65.99 - RPI 4"
+    assert c.tva_ou_pas() == False
+    assert c.taux_tva() == 0.21
+    assert c2.taux_tva() == 0.06
+    assert c.poids_total() == 0.240
+    assert c2.poids_total() == 0
+
+z = Facture("Test", [c, c, c, c], 3)
+
+def test_facture_nombre():
+    assert z.nombre(c) == 4
 
 if __name__ == "__main__":
 
@@ -55,5 +98,9 @@ if __name__ == "__main__":
 
     print("\n*** TEST DE LA CLASSE Facture ***\n")
     test_facture(facture)
+    test_facture_pp(pc_store)
 
 test_article_reparation()
+test_piece()
+test_article_piece()
+test_facture_nombre()
